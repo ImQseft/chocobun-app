9883b5168e19a076e7dbeee4059ad0488ff445c3
ImQseft <>
2018-11-02 23:15:53 +0800

    Updates

---
+++ precache-manifest.cf08c0dbfc9a314c6aa8c5708a931627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f896e5bad58603a0245",
+    "url": "/static/js/main.2f896e5b.chunk.js"
+  },
+  {
+    "revision": "7b0253f8afcb497f8aef",
+    "url": "/static/js/1.7b0253f8.chunk.js"
+  },
+  {
+    "revision": "2f896e5bad58603a0245",
+    "url": "/static/css/main.43dcb397.chunk.css"
+  },
+  {
+    "revision": "6d4ce6457a9a0b9a0f8c469d39d4bed2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chocobun-app/precache-manifest.55914f05df28aff6c0c32f326cab6395.js"
+  "/precache-manifest.cf08c0dbfc9a314c6aa8c5708a931627.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/chocobun-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
