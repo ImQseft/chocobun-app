564dc222de0ac774e53a687b950bde4454e96482
ImQseft <>
2018-12-30 22:37:42 +0800

    Updates

--- precache-manifest.59b6f2ff2d7151b31dde965a7d67a4ae.js
+++ precache-manifest.59b6f2ff2d7151b31dde965a7d67a4ae.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/chocobun-app/static/js/runtime~main.9180ac00.js"
   },
   {
-    "revision": "89194992bf2a627dfa41",
-    "url": "/chocobun-app/static/js/main.89194992.chunk.js"
+    "revision": "b832e01149d0d757e7b8",
+    "url": "/chocobun-app/static/js/main.b832e011.chunk.js"
   },
   {
     "revision": "495875e2f3ea08ce2133",
     "url": "/chocobun-app/static/js/1.495875e2.chunk.js"
   },
   {
-    "revision": "89194992bf2a627dfa41",
-    "url": "/chocobun-app/static/css/main.ced80bb3.chunk.css"
+    "revision": "b832e01149d0d757e7b8",
+    "url": "/chocobun-app/static/css/main.66e6ffe3.chunk.css"
   },
   {
-    "revision": "226455f595aa740f44b59beacf85bbaa",
+    "revision": "685fe8af9b7578e0f6d1105cd1a413f4",
     "url": "/chocobun-app/index.html"
   }
 ];
